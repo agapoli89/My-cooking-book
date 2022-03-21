b9a6cf5a35b816169ece6fa3e4db6d25d6d95beb
Aga <agapoli@gmail.com>
2022-03-21 19:09:24 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <nav>
+            <Link to ="/" className='brand'>
+                <h1>My cooking book</h1>
+            </Link>
+            <Link to="create">Create recipe</Link>
+        </nav>
+    </div>
+  )
+}
