f2c44b378368f2b0a681808929eeb0e46e34ec45
Aga <agapoli@gmail.com>
2022-05-23 18:17:06 +0200

    use ThemeContext in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
 
 import './Navbar.css'
 
 import Searchbar from './Searchbar'
 
 export default function Navbar() {
+
+  const { color } = useContext(ThemeContext)
+
   return (
-    <div className='navbar'>
+    <div className='navbar' style={{ background: color }}>
         <nav>
             <Link to ="/" className='brand'>
                 <h1>My cooking book</h1>
