a82dfa313e40481c0ea889bcecd17217847a6b37
Aga <agapoli@gmail.com>
2022-09-20 19:01:29 +0200

    install & config firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app"
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBCT_IEAhZKktPp5QxNTg8SISsKOHNLJyU",
+    authDomain: "cooking-site-4969f.firebaseapp.com",
+    projectId: "cooking-site-4969f",
+    storageBucket: "cooking-site-4969f.appspot.com",
+    messagingSenderId: "209491708121",
+    appId: "1:209491708121:web:c19e211745a4563a7916c8"
+  }
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  //init services
+  const projectFirestore = firebase/firestore()
+
+  export { projectFirestore }
