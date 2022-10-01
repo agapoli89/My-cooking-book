eeb70d704c6394757291571638ffc570ca079604
Aga <agapoli@gmail.com>
2022-10-01 18:34:46 +0200

    fix runing firestore

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig)
 
   //init services
-  const projectFirestore = firebase/firestore()
+  const projectFirestore = firebase.firestore()
 
   export { projectFirestore }
